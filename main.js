8b9ab101b8edc15e0a97947ddbac4921c1a7a633
Tharindu37 <tharindut520@gmail.com>
2023-03-16 15:24:53 +0530

    Changed the contributors load end-point

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
 // Load Contributers
-var users=["Tharindu37","BawanthaPrasad","AK4266","Navodi99","emergenitro","aayushdhakal","hiruhemapala","khushpreetsinghb","Yasanga-Nishaditha","shiva0002"];
 $(document).ready(function(){
-    function getUserDeails(username){
-        $.ajax({
-            url: "https://api.github.com/users/"+username,
-            type: 'GET',
-            dataType: 'json', // added data type
-            success: function(res) {
+    $.ajax({
+        url: "https://api.github.com/repos/Tharindu37/ProSolutions/contributors",
+        type: 'GET',
+        dataType: 'json', // added data type
+        success: function(res) {
+            console.log(res);
+            res.forEach(element => {
                 $("#contributor").append(`
-                <div class="col-sm-12 col-md-4 col-lg-3">
-                    <div class="contributor-wrap">
-                        <img src="${res.avatar_url}" alt="" class="image-wrap">
-                        <h6 class="name-wrap">${res.name==null?res.login:res.name}</h6>
+                    <div class="col-sm-12 col-md-4 col-lg-3">
+                        <div class="contributor-wrap">
+                            <img src="${element.avatar_url}" alt="" class="image-wrap">
+                            <h6 class="name-wrap">${element.name==null?element.login:res.name}</h6>
+                        </div>
                     </div>
-                </div>
                 `)
-            }
-        });
-    };
-    users.forEach(element => {
-        getUserDeails(element)
-    });
+            });
+        }
+    }); 
 });
